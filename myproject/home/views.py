fc19f616e84851987eef64ab2568dcd137d605b5
Pooja <poojaajumon@gmail.com>
2024-03-01 12:57:05 +0530

    Second commit

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Article
 from django.utils import timezone
+from django.contrib import messages
 
 def index(request):
     data={
@@ -23,7 +24,8 @@ def add(request):
         date = timezone.now()
         query=Article(head=head,para=para,image=image,date=date)
         query.save()
-        return redirect("/")
+        messages.success(request,"Details Added Successfully")
+        # return redirect("/")
     return render(request,'admin.html')
 
 def detailed(request,id):
@@ -38,32 +40,28 @@ def delete(request,id):
     dlt.delete()
     return redirect("/")
 
+
+    
 def edit(request, id):
     if request.method == "POST":
         head = request.POST.get('head')
         para = request.POST.get('para')
         image = request.FILES.get('image')
 
-        
         edit_article = Article.objects.get(id=id)
 
-       
         edit_article.head = head
         edit_article.para = para
 
-        
         if image:
             edit_article.image = image
 
-       
         edit_article.save()
 
-        return redirect("/")
+        messages.success(request, "Details Updated Successfully")
+        return render(request, 'edit.html', {'blog': edit_article})
     else:
-        
         data = {
             "blog": Article.objects.get(id=id)
         }
         return render(request, 'edit.html', data)
-
-    
